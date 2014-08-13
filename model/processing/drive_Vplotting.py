b2c0d2d68a80d8e6d2129d711d1d8838e6eeddc6
Percy Link <percy@Percys-MacBook-Pro.local>
2014-08-13 16:52:50 -0700

    loop over domains

--- model/processing/drive_Vplotting.py
+++ model/processing/drive_Vplotting.py
@@ -8,12 +8,20 @@ from utils.plot_uw_vertslice import VerticalPlotter
 
 if __name__ == "__main__":
 
-    run_name = 'CA-0.08'
+    run_name = 'DK-Aug11-mod'
     plot_interval = 3
-    file_in = 'wrfout_diff_CTRL'
-    reg_diff = 'diff'
+    file_in = 'wrfout_d01_2013-08-11_00:00:00'
+    reg_diff = 'reg'
     
-    print run_name, file_in, reg_diff, 'interval', plot_interval
+    files = ['wrfout_d01_2013-08-11_00:00:00', 'wrfout_d02_2013-08-11_00:00:00', 'wrfout_d03_2013-08-11_00:00:00']
+    domains = ['d01', 'd02', 'd03']
 
-    p = VerticalPlotter(file_in=file_in, plot_interval=plot_interval, run_name=run_name, reg_diff=reg_diff)
-    p.run()
+    for i in xrange(len(files)):
+
+        print run_name, files[i], reg_diff, 'interval', plot_interval, domains[i]
+
+        p = VerticalPlotter(file_in=files[i], plot_interval=plot_interval, run_name=run_name, \
+                reg_diff=reg_diff, lat_locs=[], lon_locs=[11.7], plot_type='quiver', domain=domains[i])
+        p.run()
+
+    #root_dir='/Users/percy/Documents/Research/Projects/wind/wind-thesis/model/output'
